be20711597e66cee59e9797abcb60ec5992d6e95
Liberty <73147145+Mhiztaliberty@users.noreply.github.com>
2021-06-23 10:55:43 +0100

    Add files via upload

---
+++ Day 2 Numpy.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+#initialize list of lists
+data = [['Ayo', 10], ['Imran', 15], ['Chucks', 14]]
+
+#create the pandas DataFrame from the list and adding column headers
+df = pd.DataFrame(data, columns = ['Name', "Age'])
+
+                                   df
+                                    
+                                   
+
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[ ]:
+
+
+#initialize list of lists
+data = [['Ayo', 10], ['Imran', 15], ['Chucks', 14]]
+
+#create the pandas DataFrame from the list and adding column headers
+
+df = pd.DataFrame(data, columns = ['Name', "Age'])
+                                   
+                                   
+
+                                   
+                                    
+
