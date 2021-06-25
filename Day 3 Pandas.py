927254eda4c05550383a94867c51331b698f19f0
Liberty <73147145+Mhiztaliberty@users.noreply.github.com>
2021-06-25 12:16:31 +0100

    Add files via upload
    Day 4 
    This is the continuation of page 4

--- Day 3 Pandas.py
+++ Day 3 Pandas.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[6]:
 
 
 import pandas as pd
 
+#Rows represent observations while columns represent input features
 
-# In[2]:
+
+# In[13]:
 
 
 data = [['Ayo', 10], ['Imran', 15], ['Chucks', 14]]
 
 df = pd.DataFrame(data, columns = ['Name', 'Age'])
-print(df)
+df
 
 
-# In[3]:
+# In[14]:
 
 
 #Create the pandas  DataFrame from the dictionary of array list 
@@ -30,10 +32,10 @@ data = {'Name' : ['Ayo', 'Imran', 'Chucks'], 'Age' :[10, 15, 14]}
 df = pd.DataFrame(data)
 
 #print DataFrame
-print(df)
+df
 
 
-# In[5]:
+# In[15]:
 
 
 #Example 2
@@ -47,24 +49,140 @@ dict_data = {"State": ["Abia", "Adamawa", "Lagos", "Osun", "Rivers"],
 
 df = pd.DataFrame(dict_data)
 
-print(df)
+df
 
 
-# In[13]:
+# In[83]:
 
 
 csv_df = pd.read_csv('Desktop/Sensor2021.csv') 
 print(csv_df)
 
 
+# In[84]:
+
+
+csv_df.describe()
+
+
+# In[25]:
+
+
+csv_df['POPULATION'].mean()
+
+
+# In[28]:
+
+
+csv_df['POPULATION'].median()
+
+
+# In[29]:
+
+
+csv_df['POPULATION'].std()
+
+
+# In[30]:
+
+
+csv_df['POPULATION'].min()
+
+
+# In[31]:
+
+
+csv_df['POPULATION'].max()
+
+
+# In[32]:
+
+
+csv_df.columns
+
+
+# In[38]:
+
+
+csv_df.info()
+
+
+# In[85]:
+
+
+print("This is State Column as Pandas Series", '\n')
+
+
+print(csv_df ['STATES'], '\n')
+
+
+
+print("This is State Column as Pandas Dataframe", '\n')
+
+print(csv_df [['STATES']], '\n')
+
+
+#This print out DataFrame with States and Population Columns"
+
+print("This is DataFrame with States and Population Column", '\n')
+
+
+print(csv_df [['STATES', 'POPULATION']])
+
+
+# In[89]:
+
+
+print(csv_df[0:10], '\n')
+
+print(csv_df[10:16])
+      
+
+
+# In[155]:
+
+
+#Using loc and Iloc
+#Since the dataset contains no label-based index, we can only use integer based ioc
+
+#Print out observation for the third state
+print(csv_df.iloc[2], '\n' '\n')
+
+#Print out observation for the 4th and 5th state
+print(csv_df.loc[[3, 4]])
+
+
+# In[189]:
+
+
+df = csv_df.drop(["CAPITAL"], axis =1)
+print(csv_df)
+
+
+# In[215]:
+
+
+df['AREA POPULATION'] = df['AREA(km2)'] + df['POPULATION']
+df.columns
+
+
+# In[212]:
+
+
+df.sort_index(inplace = True, ascending = True)
+
+df
+
+
+# In[219]:
+
+
+df = csv_df.drop(["AreaPopulation"], axis =1)
+df
+
+
 # In[ ]:
 
 
-#End of Day 3 Program
 
-
-# In[ ]:
-
-
-#Thanks
 
